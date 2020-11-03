4de9459141fe32ce80d5935dc59c8c8a3f58eb27
Muhammad Abubakar <muhammadabubakr32@gmail.com>
2020-11-03 03:53:10 -0800

    commit changes

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,6 @@ class ProjectBlog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_created=True)
     description = models.CharField(max_length=200)
+    
     def __str__(self):
         return self.title
